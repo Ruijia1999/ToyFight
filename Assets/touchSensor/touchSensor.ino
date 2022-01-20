a0ed8f43a38f7f12ac04f1adef0a63f87adc7b56
RuijiaHuang <965990240@qq.com>
2022-01-20 11:23:14 -0700

    add Arduino script

---
+++ Assets/touchSensor/touchSensor.ino
@@ -0,0 +1,20 @@
+int sensorValue = 0;
+int sensorPin = A5;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(8,255);
+  sensorValue = analogRead(sensorPin);
+  sensorValue = map(sensorValue,0,1023,0,255);
+  if(sensorValue == 255){
+    Serial.println("head");
+  }
+
+  
+  delay(10);
+}
